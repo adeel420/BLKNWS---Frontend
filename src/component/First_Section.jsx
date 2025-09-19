19b484d0b3612c2c3cdd0897bd1d234cab90a2a2
Your Name <your_email@example.com>
2025-09-20 00:37:58 +0500

    add

--- src/component/First_Section.jsx
+++ src/component/First_Section.jsx
@@ -50,7 +50,7 @@ const First_Section = () => {
             loop
             muted
             playsInline
-            className="w-[230px] h-[230px] md:w-[630px] md:h-[630px] object-cover z-[1] vedio-hero"
+            className="w-[330px] h-[330px] md:w-[630px] md:h-[630px] object-cover z-[1] vedio-hero"
           ></video>
         </div>
 
